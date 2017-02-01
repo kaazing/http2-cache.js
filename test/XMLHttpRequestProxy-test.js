2185efe9f782d9c1fbc98d45c2073aa47073d974
David Witherspoon <david.witherspoon@kaazing.com>
2017-01-31 17:16:44 -0800

    Setup using fake xhr

--- test/XMLHttpRequestProxy-test.js
+++ test/XMLHttpRequestProxy-test.js
@@ -1,24 +1,70 @@
 var assert = require('assert');
-var XMLHttpRequest = require("xhr2").XMLHttpRequest;
 var XMLHttpRequestProxy = require("../lib/XMLHttpRequestProxy");
+XMLHttpRequest = require("xhr2").XMLHttpRequest;
+var sinon = require('sinon');
 
 describe('XMLHttpRequestProxy', function () {
+
+    beforeEach(function(){
+        this.xhr = sinon.useFakeXMLHttpRequest();
+        var requests = this.requests = [];
+
+        this.xhr.onCreate = function (xhr) {
+            requests.push(xhr);
+        };
+    });
+
+    afterEach(function() {
+        this.xhr.restore();
+    });
 
     describe('.proxy()', function () {
 
         it('with empty params throws exception', function () {
-            assert.throws(new XMLHttpRequestProxy().proxy);
+            assert.throws(XMLHttpRequestProxy.prototype.proxy);
         });
 
         it('with no arrays throws exception', function () {
             assert.throws(function () {
-                    new XMLHttpRequestProxy().proxy("http://url");
+                    new XMLHttpRequestProxy.prototype.proxy("https://url");
+                }
+            )
+        });
+
+        it('with no arrays throws exception', function () {
+            assert.throws(function () {
+                    new XMLHttpRequestProxy.prototype.proxy("https://url");
                 }
             )
         });
 
         it('should load config', function () {
+            new XMLHttpRequestProxy.prototype.proxy(["https://configuration"]);
+            assert.equal(this.requests.length, 1);
+            assert.equal(this.requests[0].url, "https://configuration");
+            console.log("------");
+            console.log(this.requests[0]);
+            console.log(this.requests[0].onload("what"));
+            console.log(this.requests[0]);
+        });
+
+        it('should load multiple configs', function () {
+            new XMLHttpRequestProxy.prototype.proxy(["https://configuration", "https://configuration2"]);
+            assert.equal(this.requests.length, 2);
+            assert.equal(this.requests[0].url, "https://configuration");
+            assert.equal(this.requests[1].url, "https://configuration2");
+        });
+
+        it('should load single config', function () {
+            // XMLHttpRequest = sinon.mock(XMLHttpRequest);
+            new XMLHttpRequestProxy.prototype.proxy(["https://configuration"]);
+
+        });
+
+        it('should load multiple config', function () {
 
         });
     });
+
+
 });
