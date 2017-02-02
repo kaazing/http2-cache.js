a0e0cdf4a946e5fdbabc25d0b2d55d516ad43f0b
David Witherspoon <david.witherspoon@kaazing.com>
2017-02-01 17:49:58 -0800

    XMLHttpRequestProxy loads config and starts PushCacheClient

--- test/XMLHttpRequestProxy-test.js
+++ test/XMLHttpRequestProxy-test.js
@@ -1,24 +1,38 @@
-var assert = require('assert');
 var XMLHttpRequestProxy = require("../lib/XMLHttpRequestProxy");
-XMLHttpRequest = require("xhr2").XMLHttpRequest;
+var assert = require('assert');
 var sinon = require('sinon');
 
 describe('XMLHttpRequestProxy', function () {
 
-    beforeEach(function(){
-        this.xhr = sinon.useFakeXMLHttpRequest();
-        var requests = this.requests = [];
+    var validConfig1 = [{
+        "url": "http://push-service-url/",
+        "options": {
+            "transport": "ws://localhost"
+        }
+    }];
 
-        this.xhr.onCreate = function (xhr) {
-            requests.push(xhr);
-        };
-    });
-
-    afterEach(function() {
-        this.xhr.restore();
-    });
+    var validConfig2 = [{
+        "url": "http://push-service-url2/",
+        "options": {
+            "transport": "ws://localhost2"
+        }
+    }];
 
     describe('.proxy()', function () {
+
+        beforeEach(function () {
+            this.addStub = sinon.stub(XMLHttpRequestProxy.prototype, "_add");
+            this.xhr = sinon.useFakeXMLHttpRequest();
+            var requests = this.requests = [];
+            this.xhr.onCreate = function (xhr) {
+                requests.push(xhr);
+            };
+        });
+
+        afterEach(function () {
+            XMLHttpRequestProxy.prototype._add.restore();
+            this.xhr.restore();
+        });
 
         it('with empty params throws exception', function () {
             assert.throws(XMLHttpRequestProxy.prototype.proxy);
@@ -39,32 +53,27 @@ describe('XMLHttpRequestProxy', function () {
         });
 
         it('should load config', function () {
-            new XMLHttpRequestProxy.prototype.proxy(["https://configuration"]);
+            var xhrp = new XMLHttpRequestProxy();
+            xhrp.proxy(["https://configuration"]);
             assert.equal(this.requests.length, 1);
             assert.equal(this.requests[0].url, "https://configuration");
-            console.log("------");
-            console.log(this.requests[0]);
-            console.log(this.requests[0].onload("what"));
-            console.log(this.requests[0]);
+
+            this.requests[0].responseType = "json";
+            this.requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(validConfig1));
+            assert.ok(this.addStub.withArgs(validConfig1).calledOnce);
         });
 
         it('should load multiple configs', function () {
-            new XMLHttpRequestProxy.prototype.proxy(["https://configuration", "https://configuration2"]);
+            var xhrp = new XMLHttpRequestProxy();
+            xhrp.proxy(["https://configuration", "https://configuration2"]);
             assert.equal(this.requests.length, 2);
             assert.equal(this.requests[0].url, "https://configuration");
             assert.equal(this.requests[1].url, "https://configuration2");
-        });
-
-        it('should load single config', function () {
-            // XMLHttpRequest = sinon.mock(XMLHttpRequest);
-            new XMLHttpRequestProxy.prototype.proxy(["https://configuration"]);
-
-        });
-
-        it('should load multiple config', function () {
-
+            this.requests[0].responseType = "json";
+            this.requests[1].responseType = "json";
+            this.requests[0].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(validConfig1));
+            this.requests[1].respond(200, {'Content-Type': 'application/json'}, JSON.stringify(validConfig2));
         });
     });
-
 
 });
