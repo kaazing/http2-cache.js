501ebba576ceea9bd44492dce9fd5b631c8c0832
David Witherspoon <david.witherspoon@kaazing.com>
2017-02-13 15:05:33 -0800

    Checkpoint

---
+++ integration-test/reaktivity/ws-echo-config.js
@@ -0,0 +1,21 @@
+var InetAddress = Java.type('java.net.InetAddress');
+var HashMap = Java.type('java.util.HashMap');
+
+var headers = new HashMap();
+headers.put("upgrade", "websocket");
+headers.put(":authority", "localhost:8080");
+
+var port = 8080;
+var address = InetAddress.getByName("127.0.0.1");
+
+wsController.routeInputNew("http", 0, "ws", 0, "echo")
+    .thenCompose(function (wsInputRef) {
+        return httpController.routeInputNew("tcp", 0, "ws", wsInputRef, headers);
+    })
+    .thenCompose(function (httpInputRef) {
+        return tcpController.routeInputNew("any", port, "http", httpInputRef, address);
+    })
+    .thenAccept(function () {
+        print("WS echo bound to localhost:8080");
+    });
+
