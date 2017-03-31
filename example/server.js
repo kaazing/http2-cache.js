39505314788625b616d1e0f9fb32bb9245d3796a
David Witherspoon <david.witherspoon@kaazing.com>
2017-03-30 17:16:24 -0700

    1. Fixed race condition in configuration onPush 2. Start IT 3. Cleaned up examples

--- example/server.js
+++ example/server.js
@@ -8,171 +8,179 @@ var options = {
     cert: fs.readFileSync('./resources/localhost.crt')
 };
 
-var basePath = ['/compatibitity-check.html', 'index.html', '/']
+var basePath = ['/compatibitity-check.html', 'index.html', '/'];
 
-var test1 = {
-    'push': function (response) {
-        var body = 'success';
-        var push = response.push('/test1result');
-        push.writeHead(200,
-            {
-                'Content-Type': 'text/html',
-                'Content-Length': body.length,
-                'Cache-Control': 'no-store, max-age=5'
+var tests = [
+    {
+        'name': "SHOULD_be_on_H2",
+        'run': function (request, response) {
+            var result = "not on h2";
+            if (response.push) {
+                result = "success";
             }
-        );
-        push.end(body);
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': result.length
+            });
+            response.end(result);
+        }
+    },
+    {
+        'name': "SHOULD_cache_on_cache-able_push_promise",
+        'before': function (request, response) {
+            var body = 'success';
+            var push = response.push('/SHOULD_cache_on_cache-able_push_promise');
+            push.writeHead(200,
+                {
+                    'Content-Type': 'text/html',
+                    'Content-Length': body.length,
+                    'Cache-Control': 'no-store, max-age=5'
+                }
+            );
+            push.end(body);
+        },
+        'run': function (request, response) {
+            var result = 'fail: should never request this';
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': result.length
+            });
+            response.end(result);
+        }
+    },
+    {
+        'name': 'MAY_cancel_cache-able_push_promise_if_already_has_it_cached',
+        'mappings': {},
+        'run': function (request, response) {
+            var testId = parseUrl(request.url).query.split("=")[1];
+            if (!this.mappings[testId]) {
+                this.mappings[testId] = 1;
+            } else {
+                this.mappings[testId]++;
+            }
+            var body = 'success' + this.mappings[testId];
+            var push = response.push('/MAY_cancel_cache-able_push_promise_if_already_has_it_cached' + testId);
+            push.writeHead(200,
+                {
+                    'Content-Type': 'text/html',
+                    'Content-Length': body.length,
+                    'Cache-Control': 'no-store, max-age=5',
+                    'Date:': getDate()
+                }
+            );
+            push.end(body);
+
+            var message = 'sent push promises';
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': message.length,
+                'Date:': getDate()
+            });
+            response.end(message);
+        }
+    },
+
+    {
+        'name': 'SHOULD_bypass_cache_on_push_promise_with_no-cache',
+        'mappings': {},
+        'run': function (request, response) {
+            var testId = parseUrl(request.url).query.split("=")[1];
+            if (!this.mappings[testId]) {
+                this.mappings[testId] = 1;
+            } else {
+                this.mappings[testId]++;
+            }
+            var body = 'success' + this.mappings[testId];
+            var push = response.push(
+                {
+                    path: '/SHOULD_bypass_cache_on_push_promise_with_no-cache' + testId,
+                    headers: {'Cache-Control': 'no-cache'}
+                }
+            );
+
+            var date = getDate();
+            push.writeHead(200,
+                {
+                    'Content-Type': 'text/html',
+                    'Content-Length': body.length,
+                    'Cache-Control': 'no-store, max-age=5',
+                    'Date:': date
+                }
+            );
+            console.log("sent push with date: " + date + " for /test3result_" + testId + " with body: " + body);
+            push.end(body);
+
+            var message = 'sent push promises';
+            response.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': message.length,
+                'Date:': getDate()
+            });
+            response.end(message);
+        }
     }
-};
+];
 
-var test2 = {
-    idMapping: {}
-};
-
-function getDate(){
+function getDate() {
     return new Date();
 }
 
-var testbase = {
-    'response': function (request, response) {
-        // TODO, SECURITY BUG IN NOT CHECKING USER DATA AND THEN ECHOING, USE ONLY FOR LOCAL TESTING
-        // console.log(request.url);
-        if (basePath.indexOf(request.url) > -1) {
-            // send result
-            test1.push(response);
-            var filePath = path.join(__dirname, 'test.html');
-            var stat = fs.statSync(filePath);
-            response.writeHead(200, {
-                'Content-Type': 'text/html',
-                'Content-Length': stat.size
-            });
-            var readStream = fs.createReadStream(filePath);
-            readStream.pipe(response);
-        } else if (request.url === '/favicon.ico') {
-            var str = 'Not favicon';
-            response.writeHead(404, {
-                'Content-Type': 'text/html',
-                'Content-Length': str.length
-            });
-            response.end(str);
-        } else {
-            var pathname = parseUrl(request.url).pathname;
-
-
-
-            switch (pathname) {
-                // SHOULD be on H2
-                case '/test0result':
-                    var result = "not on h2";
-                    if (response.push) {
-                        result = "success";
-                    }
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html',
-                        'Content-Length': result.length
-                    });
-                    response.end(result);
-                    break;
-
-                // SHOULD cache on cache-able push promise
-                case '/test1result':
-                    var result = 'fail: should never request this';
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html',
-                        'Content-Length': result.length
-                    });
-                    response.end(result);
-                    break;
-
-                // MAY cancel cache-able push promise if already has it cached
-                case '/test2start':
-                    var testId = parseUrl(request.url).query.split("=")[1];
-                    if (!test2.idMapping[testId]) {
-                        test2.idMapping[testId] = 1;
-                    } else {
-                        test2.idMapping[testId]++;
-                    }
-                    // console.log(testId + " sent push " + test2.idMapping[testId] + " times")
-                    var body = 'success' + test2.idMapping[testId];
-                    var push = response.push('/test2result_' + testId);
-                    push.writeHead(200,
-                        {
-                            'Content-Type': 'text/html',
-                            'Content-Length': body.length,
-                            'Cache-Control': 'no-store, max-age=5',
-                            'Date:' : getDate()
-                        }
-                    );
-                    push.end(body);
-
-
-                    var message = 'sent push promises';
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html',
-                        'Content-Length': message.length,
-                        'Date:' : getDate()
-                    });
-                    response.end(message);
-                    break;
-                // SHOULD bypass cache on push promise with no-cache
-                case '/test3start':
-                    var testId = parseUrl(request.url).query.split("=")[1];
-                    if (!test2.idMapping[testId]) {
-                        test2.idMapping[testId] = 1;
-                    } else {
-                        test2.idMapping[testId]++;
-                    }
-                    // console.log(testId + " sent push " + test2.idMapping[testId] + " times");
-                    var body = 'success' + test2.idMapping[testId];
-                    var push = response.push(
-                        {
-                            path: '/test3result_' + testId,
-                            headers: {'Cache-Control': 'no-cache'}
-                        }
-                    );
-
-                    var date = getDate();
-                    push.writeHead(200,
-                        {
-                            'Content-Type': 'text/html',
-                            'Content-Length': body.length,
-                            'Cache-Control': 'no-store, max-age=5',
-                            'Date:' : date
-                        }
-                    );
-                    console.log("sent push with date: " + date + " for /test3result_" + testId + " with body: " + body);
-                    push.end(body);
-
-                    var message = 'sent push promises';
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html',
-                        'Content-Length': message.length,
-                        'Date:' : getDate()
-                    });
-                    response.end(message);
-                    break;
-                // SHOULD support "Long Push"
-                default:
-                    // test2: interesting firefox behavior
-                    console.log("got unexpected request indicating bug for:" + request.url);
-                    if (pathname.indexOf('test2result') > -1 || pathname.indexOf('test3result')) {
-                        var message = 'FAIL / BUG (Ignored PUSH PROMISES!!)';
-                        response.writeHead(200, {
-                            'Content-Type': 'text/html',
-                            'Content-Length': message.length
-                        });
-                        response.end(message);
-                    } else {
-                        // error out
-                        console.log("got unexpected request " + pathname);
-                        throw new Error('this should be overloaded in all tests');
-                    }
-
+http2.createServer(options, function (request, response) {
+    // TODO, SECURITY BUG IN NOT CHECKING USER DATA AND THEN ECHOING, USE ONLY FOR LOCAL TESTING
+    if (basePath.indexOf(request.url) > -1) {
+        // send result
+        var lengthI = tests.length;
+        for (var i = 0; i < lengthI; i++) {
+            if (tests[i].before) {
+                tests[i].before(request, response);
             }
         }
+
+        var filePath = path.join(__dirname, 'index.html');
+        var stat = fs.statSync(filePath);
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': stat.size
+        });
+        var readStream = fs.createReadStream(filePath);
+        readStream.pipe(response);
+    } else if (request.url === '/favicon.ico') {
+        var str = 'Not favicon';
+        response.writeHead(404, {
+            'Content-Type': 'text/html',
+            'Content-Length': str.length
+        });
+        response.end(str);
+    } else {
+        var pathname = parseUrl(request.url).pathname;
+        var success = false;
+        var lengthI = tests.length;
+        for (var i = 0; i < lengthI; i++) {
+            if ('/' + tests[i].name === pathname) {
+                console.log("Running test logic: " + tests[i].name);
+                tests[i].run(request, response);
+                success = true;
+                break;
+            }
+        }
+        if (!success) {
+            console.log("got unexpected request indicating bug for:" + request.url);
+            if (pathname.indexOf('test2result') > -1 || pathname.indexOf('test3result')) {
+                var message = 'FAIL / BUG (Ignored PUSH PROMISES!!)';
+                response.writeHead(200, {
+                    'Content-Type': 'text/html',
+                    'Content-Length': message.length
+                });
+                response.end(message);
+            } else {
+                // error out
+                console.log("got unexpected request " + pathname);
+                throw new Error('this should be overloaded in all tests');
+            }
+        }
+
     }
-};
+}).listen(8080);
 
-http2.createServer(options, testbase.response).listen(8080);
-
+console.log("Listening on 8080");
 
--- integration-test/http2-cache-itest.js
+++ integration-test/http2-cache-itest.js
@@ -1,3 +1,4 @@
+/* jshint ignore:start */
 if (typeof exports !== 'undefined') {
     if (typeof XMLHttpRequest === 'undefined') {
         XMLHttpRequest = require("xhr2").XMLHttpRequest;
@@ -9,15 +10,54 @@ if (typeof exports !== 'undefined') {
     XMLHttpRequest = Window.XMLHttpRequest;
     WebSocket = Window.WebSocket;
 }
-var sinon = require('sinon');
-var chai = require('chai');
+/* jshint ignore:end */
+var assert = require('assert'),
+    http = require('http');
+
 require("../lib/http2-proxy.js");
 
 describe('http2-proxy', function () {
 
-    it('long.pushing.with.cache.busting', function (done) {
+    var configServer;
+
+    before(function (done) {
+        configServer = http.createServer(function (request, response) {
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({
+                'url': 'http://localhost:8080/',
+                'options': {
+                    'transport': 'ws://localhost:8080/'
+                }
+            }));
+        });
+        configServer.listen(8081, done);
+    });
+
+    after(function (done) {
+        configServer.close(done);
+    });
+
+    it('should.support.long.pushing', function (done) {
         XMLHttpRequest.proxy(["http://localhost:8081/config1"]);
-        XMLHttpRequest
+
+        var cnt = 0;
+        var interval = setInterval(function () {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener("load", function () {
+                cnt++;
+                assert.equal(xhr.responseText, "response" + cnt);
+                if (cnt === 3) {
+                    clearInterval(interval);
+                    done();
+                }
+            });
+            xhr.open("GET", "http://localhost:8080/");
+            xhr.send();
+
+        });
+
+
+        done();
     });
 
 
