9fd27b0e3e09c6f2cadb4a3908e67ebb84ab2eea
Harold Thetiot <hthetiot@gmail.com>
2017-08-17 14:15:25 -0700

    split push and simple xhr tests

--- test/http2-xhr-test.js
+++ test/http2-xhr-test.js
@@ -14,27 +14,12 @@ var assert = require('assert'),
 
 describe('H2 XHR', function () {
 
-    var config1 = {
-        'push': 'http://cache-endpoint1/stream',
-        'transport': 'ws://localhost:7081/',
-        'proxy': [
-            'http://cache-endpoint1/',
-            'http://cache-endpoint4/'
-        ]
-    };
-
     var config2 = {
         'transport': 'ws://localhost:7082/path',
         'proxy': [
             'http://cache-endpoint2/',
-            'http://cache-endpoint3/'
-        ]
-    };
-
-    var config3 = {
-        'transport': 'ws://localhost:7082/path',
-        'proxy': [
-            'http://localhost:7080/path/proxy'
+            'http://cache-endpoint3/',
+            'http://localhost:7080/path/proxy',
         ]
     };
 
@@ -45,15 +30,9 @@ describe('H2 XHR', function () {
 
             var path = request.url;
             //console.log('configServer', path);
-            if (path === '/config1') {
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                response.end(JSON.stringify(config1));
-            } else if (path === '/config2') {
+            if (path === '/config2') {
                 response.writeHead(200, {'Content-Type': 'application/json'});
                 response.end(JSON.stringify(config2));
-            } else if (path === '/config3') {
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                response.end(JSON.stringify(config3));
             } else if (path === '/headers') {
                 response.writeHead(200, {'Content-Type': 'application/json'});
 
@@ -97,57 +76,23 @@ describe('H2 XHR', function () {
         configServer.close(done);
     });
 
-
-    var s1;
     var s2;
-    var s1OnRequest;
     var s2OnRequest;
 
     beforeEach(function (done) {
-        // starts the 2 h2overWs servers
-        s1OnRequest = function (request, response) {
-            throw new Error("s1OnRequest Unexpected request: " + request.url);
-        };
-
         s2OnRequest = function (request, response) {
             throw new Error("s2OnRequest Unexpected request: " + request.url);
         };
-
-        var completed = 0;
-
-        function doneOn2() {
-            completed++;
-            if (completed === 2) {
-                done();
-            }
-        }
-
-        // start config1 http2 server
-        s1 = http2.raw.createServer(getWSTransportServer(), function (request, response) {
-            s1OnRequest(request, response);
-        });
-        s1.listen(7081, doneOn2);
 
         // start config2 http2 server
         s2 = http2.raw.createServer(getWSTransportServer(), function (request, response) {
             s2OnRequest(request, response);
         });
-        s2.listen(7082, doneOn2);
+        s2.listen(7082, done);
     });
 
     afterEach(function (done) {
-
-        var completed = 0;
-
-        function doneOn2() {
-            completed++;
-            if (completed === 2) {
-                done();
-            }
-        }
-
-        s1.close(doneOn2);
-        s2.close(doneOn2);
+        s2.close(done);
     });
 
     it('should proxy GET request', function (done) {
@@ -270,74 +215,8 @@ describe('H2 XHR', function () {
 
     });
 
-    it('should not proxy different origin GET requests and pass headers', function (done) {
-        XMLHttpRequest.proxy(["http://localhost:7080/config2"]);
-        var xhr = new XMLHttpRequest();
-        var xhr2 = new XMLHttpRequest();
-
-        var doneCnt = 0;
-
-        function doneN(n) {
-            if (++doneCnt === n) {
-                done();
-            }
-        }
-
-        var statechanges = 0;
-        xhr.onreadystatechange = function () {
-            assert.equal(xhr.readyState, statechanges++);
-            if (xhr.readyState >= 2) {
-                assert.equal(200, xhr.status);
-                assert.equal("OK", xhr.statusText);
-            }
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                assert.equal(JSON.stringify({
-                    "content-type": "application/json",
-                    "x-custom-header": "MyValue",
-                    "connection": "keep-alive",
-                    "host": "localhost:7080",
-                    "content-length": "0"
-                }), xhr.responseText);
-                doneN(3);
-            }
-        };
-
-        xhr.onloadstart = function () {
-            xhr.onprogress = function () {
-                xhr.onload = function () {
-                    xhr.onloadend = function () {
-                        doneN(3);
-                    };
-                };
-            };
-        };
-
-        var statechanges2 = 0;
-        xhr2.onreadystatechange = function () {
-             assert.equal(xhr2.readyState, statechanges2++);
-            if (xhr2.readyState >= 2) {
-                assert.equal(xhr2.status, 200);
-                assert.equal(xhr2.statusText, "OK");
-            }
-            if (xhr2.readyState === 4 && xhr2.status === 200) {
-                xhr2.addEventListener('load', function () {
-                    assert.equal(JSON.stringify(config1), xhr2.responseText);
-                    doneN(3);
-                });
-            }
-        };
-
-        xhr.open('GET', 'http://localhost:7080/headers', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('X-Custom-Header', 'MyValue');
-
-        xhr.send(null);
-        xhr2.open('GET', 'http://localhost:7080/config1', true);
-        xhr2.send(null);
-    });
-
     it('should only proxy path match GET requests', function (done) {
-        XMLHttpRequest.proxy(["http://localhost:7080/config3"]);
+        XMLHttpRequest.proxy(["http://localhost:7080/config2"]);
         var message = "Affirmative, Dave. I read you. ";
         var requestCount = 0;
         s2OnRequest = function (request, response) {
@@ -407,7 +286,7 @@ describe('H2 XHR', function () {
     });
 
     it('should only proxy path match POST requests', function (done) {
-        XMLHttpRequest.proxy(["http://localhost:7080/config3"]);
+        XMLHttpRequest.proxy(["http://localhost:7080/config2"]);
     
         var formData = new FormData();
         formData.append('username', 'Chris');
@@ -493,54 +372,6 @@ describe('H2 XHR', function () {
         xhr.send(formData);
         xhr2.open('POST', 'http://localhost:7080/path/notproxy?query=1', true);
         xhr2.send(formData);
-    });
-
-    it('should use pushed results in cache', function (done) {
-        var message = "Affirmative, Dave. I read you. ";
-        var xhr = new XMLHttpRequest();
-        s1OnRequest = function (request, response) {
-            assert.equal(request.url, '/stream', 'should be on streaming url');
-            var pr = response.push({
-                'path': '/pushedCache1',
-                'protocol': 'http:'
-            });
-            pr.setHeader('Content-Type', 'text/html');
-            pr.setHeader('Content-Length', message.length);
-            pr.setHeader('Cache-Control', 'max-age=500');
-            pr.setHeader('Date', new Date());
-            pr.write(message);
-            pr.end();
-            var statechanges = 0;
-            xhr.onreadystatechange = function () {
-                ++statechanges;
-                // TODO !=1 is due to bug
-                if(statechanges !== 1) {
-                    assert.equal(xhr.readyState, statechanges);
-                }
-                if (xhr.readyState >= 2) {
-                    assert.equal(xhr.status, 200);
-                    assert.equal(xhr.statusText, "OK");
-                }
-
-                if (xhr.readyState >= 3) {
-                    assert.equal(xhr.response, message);
-                }
-
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    assert.equal(xhr.getResponseHeader('content-type'), 'text/html');
-                    assert.equal(xhr.getAllResponseHeaders()['content-type'], 'text/html');
-                    done();
-                }
-            };
-            xhr.open('GET', 'http://cache-endpoint1/pushedCache1', true);
-
-            // There is a race between xhr.js and push with out subscribe
-            xhr.subscribe(function () {
-                xhr.unsubscribe();
-                xhr.send(null);
-            });
-        };
-        XMLHttpRequest.proxy(["http://localhost:7080/config1"]);
     });
 
     it('should cache GET request and reuse (with default port)', function (done) {
@@ -670,79 +501,6 @@ describe('H2 XHR', function () {
         firstRequest.open('GET', 'http://cache-endpoint2:80/cachedGetRequestWithPort', true);
 
         firstRequest.send(null);
-    });
-
-    it('should cache GET request and re-call onreadystatechange on pushed update', function (done) {
-        var messages = [
-            "Hello, Dave. You're looking well today.",
-            "Do you want to be my friend, Dave ?"
-        ];
-        var requestCount = 0;
-        s1OnRequest = function (request, response) {
-            if (++requestCount === 1) {
-                // TODO check request headers and requests responses
-                assert.equal(request.url, '/stream');
-                //assert.equal(request.headers['x-retry-after'], 10);
-
-                var pr = response.push({
-                    'path': '/cachedGetRequestWithPush',
-                    'protocol': 'http:'
-                });
-                pr.setHeader('Content-Type', 'text/html');
-                pr.setHeader('Content-Length', messages[0].length);
-                pr.setHeader('Cache-Control', 'max-age=500');
-                pr.setHeader('Date', new Date());
-                pr.write(messages[0]);
-                pr.end();
-
-                setTimeout(function () {
-                    var pr = response.push({
-                        'path': '/cachedGetRequestWithPush',
-                        'protocol': 'http:'
-                    });
-                    pr.setHeader('Content-Type', 'text/html');
-                    pr.setHeader('Content-Length', messages[1].length);
-                    pr.setHeader('Cache-Control', 'max-age=500');
-                    pr.setHeader('Date', new Date());
-                    pr.write(messages[1]);
-                    pr.end();
-                }, 1000);
-            } else {
-                throw new Error("Should only get 1 request");
-            }
-        };
-        XMLHttpRequest.proxy(["http://localhost:7080/config1"]);
-        var firstRequest = new XMLHttpRequest();
-
-        var statechanges = 0,
-            statecomplete = 0;
-        firstRequest.onreadystatechange = function () {
-            ++statechanges;
-            
-            if (firstRequest.readyState >= 2) {
-                assert.equal(firstRequest.status, 200);
-                assert.equal(firstRequest.statusText, "OK");
-            }
-            if (firstRequest.readyState === 3) {
-                assert.equal(firstRequest.response, messages[statecomplete]);
-            }
-            if (firstRequest.readyState === 4 && firstRequest.status === 200) {
-                assert.equal(firstRequest.response, messages[statecomplete]);
-                statecomplete++;
-            }
-
-            if (statecomplete === 1) {
-                done();
-            }
-        };
-
-        firstRequest.open('GET', 'http://cache-endpoint1/cachedGetRequestWithPush', true);
-        firstRequest.setRequestHeader('X-Retry-After', 1);
-
-        // There is a race between xhr.js and push with out subscribe
-        firstRequest.subscribe(function () {
-            firstRequest.send(null);
-        });
     });
 
     it('should cache GET request and reuse for response larger than MAX_PAYLOAD_SIZE', function (done) {
