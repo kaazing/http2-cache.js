a3587cdbc46ff4b80d1706093bf24d8e667c66ce
Harold Thetiot <hthetiot@gmail.com>
2017-11-17 10:34:54 -0800

    remove e2e-test draft

--- e2e-test/server.js
+++
@@ -1,8 +0,0 @@
-/* globals console, process */
-
-var http = require('http');
-
-var getWSTransportServer = require('./../test/test-utils').getWSTransportServer,
-    getHTTPConfigServer = require('./../test/test-utils').getHTTPConfigServer;
-
-// TODO
