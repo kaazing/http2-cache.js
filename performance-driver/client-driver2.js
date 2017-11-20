9c3b17ffc0a6c2ac97fb14259be3dfb47e653b49
AdrianCozma <adrian.cozma@kaazing.com>
2017-11-20 15:45:29 +0200

    updated client for perf test

--- performance-driver/client-driver2.js
+++ performance-driver/client-driver2.js
@@ -1,21 +1,28 @@
+var url = require('url');
 var http2 = require('http2.js');
+var websocket = require('websocket-stream');
+var Agent = require('../lib/agent').Agent;
+var logger = require('../lib/logger');
+var parseUrl = require('../lib/utils').parseUrl;
 
-function Client(pushUri, transportUri){
+function Client(pushUri, transportUri) {
     var self = this;
     self.state = "INIT";
     self.pushUri = pushUri;
     self.transportUri = transportUri;
 
     self.transport = websocket(transportUri, "h2", {
-        perMessageDeflate: this.debug === false
+        perMessageDeflate : this.debug === false
     });
 
     self.transport.on('close', function () {
-        self.state = "ERROR (transport close)"
+        self.state = "CLOSE (transport close)";
     });
 
+    self._log = logger.consoleLogger;
+
     self.agent = new Agent({
-        log: self._log
+        log : self._log
     });
 
     self.cache = {};
@@ -25,38 +32,44 @@ function Client(pushUri, transportUri){
     self.openPushPromiseStream();
 }
 
-cp = Client.prototype;
+var cp = Client.prototype;
 
-cp.onPush = function(pushRequest)
-{
+cp.onPush = function (pushRequest) {
     var self = this;
     var requestPath = pushRequest.url.path;
     self.cache[requestPath] = "revalidating";
 
-    pushRequest.on('end', function(){
+    pushRequest.on('end', function () {
         self.cache[requestPath] = "not-revalidating";
     });
 
-    pushRequest.on('error', function(){
+    pushRequest.on('error', function () {
         self.cache[requestPath] = "not-revalidating";
     });
 };
 
-cp.openPushPromiseStream = function(){
+cp.openPushPromiseStream = function () {
     var self = this;
 
+    self._log.info("Push channel will open: " + self.pushUri.href);
+
     var request = http2.raw.request({
-        hostname: pushUri.hostname,
-        port: pushUri.port,
-        path: pushUri.path,
-        transportUrl: transportUri,
-        transport: transport,
-        agent: self.agent
+        hostname : self.pushUri.hostname,
+        port : self.pushUri.port,
+        path : self.pushUri.path,
+        transportUrl : self.transportUri,
+        transport : self.transport,
+        agent : self.agent
     }, function (response) {
+        self._log.info("Push channel opened: " + self.pushUri.href);
+
+        response.on('data', function (data) {
+            self._log.info("data is: " + data);
+        });
 
         response.on('finish', function () {
             // TODO consider throwing hard exception?
-            self.state = "ERROR (push finish)\"";
+            self.state = "FINISH (push finish)\"";
         });
 
         response.on('error', function () {
@@ -70,42 +83,42 @@ cp.openPushPromiseStream = function(){
 
     });
 
-    request.on('push', function(pushRequest) {
+//    request.on('response', function(response) {
+//        self._log.info("respone is: " + response);
+//      });
+
+    request.on('push', function (pushRequest) {//where does pushRequest come from?
+        self._log.info("state is: " + self.state);
         self.onPush(pushRequest);
     });
+
+    request.end();
 };
 
-cp.request = function(url)
-{
+cp.request = function (url) {
     var self = this;
     var stats = self.urlStats[url.path];
-    if (!stats)
-    {
+    if (!stats) {
         stats = self.urlStats[url.path] = {};
     }
     // TODO start time
-
-    if (self.cache[url.path] === "revalidating")
-    {
+    if (self.cache[url.path] === "revalidating") {
         // TODO record cache hit
-    }
-    else
-    {
+    } else {
         // TODO record cache miss
 
         var request = http2.raw.request({
-            hostname: url.hostname,
-            port: url.port,
-            path: url.path,
-            transportUrl: transportUri,
-            transport: transport,
-            agent: self.agent
+            hostname : url.hostname,
+            port : url.port,
+            path : url.path,
+            transportUrl : self.transportUri,
+            transport : self.transport,
+            agent : self.agent
         }, function (response) {
 
             response.on('finish', function () {
                 // TODO end time and record response //
                 stats['success'] = stats['success'] ? stats['success']++ : 1;
-                self.state = "ERROR (push finish)\"";
             });
 
             response.on('error', function () {
@@ -119,89 +132,48 @@ cp.request = function(url)
 
         });
 
-        request.on('push', function(pushRequest) {
+        request.on('push', function (pushRequest) {//where does pushRequest come from?
             self.onPush(pushRequest);
         });
+
+        request.end();
     }
 };
 
-cp.poll = function(url, interval)
-{
+cp.poll = function (url, interval) {
     var self = this;
-    self.intervals[url.path] = setInterval(
-        function () {
-            self.request(url)
-        }, interval
-    )
+    self.intervals[url.path] = setInterval(function () {
+        self.request(url);
+    }, interval);
 };
 
-var parseUri = function (href) {
-    var uri = (href instanceof url.constructor) ? href : url.parse(href);
-    uri.port = resolvePort(uri);
-
-
-    if (
-        uri.hostname === null &&
-        typeof window !== 'undefined'
-    ) {
-        uri.protocol = window.location.protocol;
-        uri.hostname = window.location.hostname;
-        uri.port = window.location.port;
-        uri.host = uri.hostname + ':' + uri.port;
-        uri.href = uri.protocol + "//" + uri.host + uri.href;
-    }
-
-    // Define uri.origin
-    uri.origin = uri.hostname + ":" + uri.port;
-
-    // Check if host match origin (example.com vs example.com:80)
-    if (uri.host !== uri.origin) {
-        // Fix href to include default port
-        uri.href = uri.href.replace(uri.protocol + "//" + uri.host, uri.protocol + "//" + uri.origin);
-        // Fix host to include default port
-        uri.host = uri.hostname + ":" + uri.port;
-    }
-
-    return uri;
-};
-
-var NUM_OF_CLIENTS = 2;
-var pushUri = parseUri("https://");
-var transportUri = parseUri("wss://");
-var testURL1 = parseUri("wss://");
+var NUM_OF_CLIENTS = 1;
+var pushUri = parseUrl("https://rest-accelerator.example.com:8081/event-stream");
+var transportUri = parseUrl("wss://rest-accelerator.example.com:8081/");
+var testURL1 = parseUrl("https://origin-server.example.com:8080/sizeof100");
 var clients = [];
 
-for (var i = 0; i < NUM_OF_CLIENTS; i++)
-{
+for (var i = 0; i < NUM_OF_CLIENTS; i++) {
     // init
     clients.push(new Client(pushUri, transportUri));
 }
 
-for (var i = 0; i < NUM_OF_CLIENTS; i++)
-{
+for (var i = 0; i < NUM_OF_CLIENTS; i++) {
     // init
-    clients[i].poll(testURL1, 2000);
-}
-for (var i = 0; i < NUM_OF_CLIENTS; i++)
-{
-    // init
-    clients[i].poll(testURL2, 2000);
+    clients[i].poll(testURL1, 5000);
 }
 
-function processResults(){
-    for(var i = 0; i < NUM_OF_CLIENTS; i++)
-    {
+function processResults() {
+    for (var i = 0; i < NUM_OF_CLIENTS; i++) {
         var client = clients[i];
-        if (client.state == "RUNNING")
-        {
+        if (client.state === "RUNNING") {
             // TODO get stats
-        }
-        else {
+        } else {
             // TODO error
         }
     }
 }
 
-setTimeout(function(){
+setTimeout(function () {
     processResults();
 });
