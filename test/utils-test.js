2a59523469b47b29fde260133e4e6fb22ab4bfb0
Harold Thetiot <hthetiot@gmail.com>
2017-12-28 19:33:50 +0100

    fix indent

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,10 +1,10 @@
 /* global console, global */
 var mergeTypedArrays = require('../lib/utils').mergeTypedArrays,
-	Utf8ArrayToStr = require('../lib/utils').Utf8ArrayToStr,
-	parseUrl = require('../lib/utils').parseUrl,
+    Utf8ArrayToStr = require('../lib/utils').Utf8ArrayToStr,
+    parseUrl = require('../lib/utils').parseUrl,
     runningInWorker = require('../lib/utils').runningInWorker,
-	FormData = require('../lib/form-data').FormData,
-	serializeXhrBody = require('../lib/utils').serializeXhrBody,
+    FormData = require('../lib/form-data').FormData,
+    serializeXhrBody = require('../lib/utils').serializeXhrBody,
     unicodeStringToTypedArray = require('./test-utils').unicodeStringToTypedArray,
     generateRandAlphaNumStr = require('./test-utils').generateRandAlphaNumStr;
     
@@ -13,85 +13,85 @@ var assert = require('assert');
 
 describe('utils', function () {
 
-	describe('runningInWorker', function () {
-		assert.equal(runningInWorker(), false)
-	});
+    describe('runningInWorker', function () {
+        assert.equal(runningInWorker(), false)
+    });
 
-	describe('parseUrl', function () {
-		it('should parse url with custom port', function () {
-			var url = "https://example.com:8080/path?query=1",
-				uri = parseUrl(url);
-			assert.equal(uri.port, 8080);	
-			assert.equal(uri.host, uri.hostname + ":" + uri.port);
-			assert.equal(uri.href, url);
-		});
+    describe('parseUrl', function () {
+        it('should parse url with custom port', function () {
+            var url = "https://example.com:8080/path?query=1",
+                uri = parseUrl(url);
+            assert.equal(uri.port, 8080);   
+            assert.equal(uri.host, uri.hostname + ":" + uri.port);
+            assert.equal(uri.href, url);
+        });
 
-		it('should parse url with default https port', function () {
-			var url = "https://example.com/path?query=1",
-				uri = parseUrl(url);
-			assert.equal(uri.port, 443);	
-			assert.equal(uri.host, uri.hostname + ":" + uri.port);
-			assert.equal(uri.href, url.replace(uri.hostname, uri.host));
-		});
+        it('should parse url with default https port', function () {
+            var url = "https://example.com/path?query=1",
+                uri = parseUrl(url);
+            assert.equal(uri.port, 443);    
+            assert.equal(uri.host, uri.hostname + ":" + uri.port);
+            assert.equal(uri.href, url.replace(uri.hostname, uri.host));
+        });
 
-		it('should parse url with default http port', function () {
-			var url = "http://example.com/path?query=1",
-				uri = parseUrl(url);
+        it('should parse url with default http port', function () {
+            var url = "http://example.com/path?query=1",
+                uri = parseUrl(url);
 
-			assert.equal(uri.port, 80);	
-			assert.equal(uri.host, uri.hostname + ":" + uri.port);
-			assert.equal(uri.href, url.replace(uri.hostname, uri.host));		
-		});
+            assert.equal(uri.port, 80); 
+            assert.equal(uri.host, uri.hostname + ":" + uri.port);
+            assert.equal(uri.href, url.replace(uri.hostname, uri.host));        
+        });
 
 
-		it('should add defult protocol, hostname, and port', function () {
+        it('should add defult protocol, hostname, and port', function () {
 
-			global.window = {
-				location: {
-					hostname: 'example.com',
-					protocol: 'https:',
-					port: '8080'
-				}
-			};
+            global.window = {
+                location: {
+                    hostname: 'example.com',
+                    protocol: 'https:',
+                    port: '8080'
+                }
+            };
 
-			var url = "/path?query=1",
-				uri = parseUrl(url);
-			assert.equal(uri.port, 8080);	
-			assert.equal(uri.host, 'example.com:8080');
-			assert.equal(uri.href, "https://example.com:8080/path?query=1");
-		});
-	});	
+            var url = "/path?query=1",
+                uri = parseUrl(url);
+            assert.equal(uri.port, 8080);   
+            assert.equal(uri.host, 'example.com:8080');
+            assert.equal(uri.href, "https://example.com:8080/path?query=1");
+        });
+    }); 
 
-	describe('Utf8ArrayToStr', function () {
-		it('should convert Utf8Array to string', function () {
-			var aStr = generateRandAlphaNumStr(2500),
-				a = unicodeStringToTypedArray(aStr);
-			assert.equal(Utf8ArrayToStr(a), aStr);
-		});
-		it('should handle 4+ byte sequences', function () {
-			assert.equal(Utf8ArrayToStr([240,159,154,133]), '🚅');
-			assert.equal(Utf8ArrayToStr([226,152,131]), '☃');
-		});
-	});	
+    describe('Utf8ArrayToStr', function () {
+        it('should convert Utf8Array to string', function () {
+            var aStr = generateRandAlphaNumStr(2500),
+                a = unicodeStringToTypedArray(aStr);
+            assert.equal(Utf8ArrayToStr(a), aStr);
+        });
+        it('should handle 4+ byte sequences', function () {
+            assert.equal(Utf8ArrayToStr([240,159,154,133]), '🚅');
+            assert.equal(Utf8ArrayToStr([226,152,131]), '☃');
+        });
+    }); 
 
-	describe('mergeTypedArrays', function () {
-		it('should merge Utf8Array', function () {
-		 	var aStr = generateRandAlphaNumStr(2500),
-		 		bStr = generateRandAlphaNumStr(2500),
-		 		a = unicodeStringToTypedArray(aStr),
-		 		b = unicodeStringToTypedArray(bStr),
-		 		c = unicodeStringToTypedArray(aStr + bStr);
-	        assert.equal(Utf8ArrayToStr(mergeTypedArrays(a, b)), Utf8ArrayToStr(c));
-	    });
-	});
+    describe('mergeTypedArrays', function () {
+        it('should merge Utf8Array', function () {
+            var aStr = generateRandAlphaNumStr(2500),
+                bStr = generateRandAlphaNumStr(2500),
+                a = unicodeStringToTypedArray(aStr),
+                b = unicodeStringToTypedArray(bStr),
+                c = unicodeStringToTypedArray(aStr + bStr);
+            assert.equal(Utf8ArrayToStr(mergeTypedArrays(a, b)), Utf8ArrayToStr(c));
+        });
+    });
 
-	describe('serializeXhrBody', function () {
-		it('should merge serialize Xhr Body', function () {
-		 	var formData = new FormData();
-	        formData.append('username', 'Chris');
-	        formData.append('username', 'Bob');
-	        formData.append('gender', 'male');  
-	        assert.equal(serializeXhrBody({}, formData), "username=Chris&username=Bob&gender=male");
-	    });
-	});
+    describe('serializeXhrBody', function () {
+        it('should merge serialize Xhr Body', function () {
+            var formData = new FormData();
+            formData.append('username', 'Chris');
+            formData.append('username', 'Bob');
+            formData.append('gender', 'male');  
+            assert.equal(serializeXhrBody({}, formData), "username=Chris&username=Bob&gender=male");
+        });
+    });
 });
